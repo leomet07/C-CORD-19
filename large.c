2161d822cd267ffaa1032a843e95a84e253a854b
leomet07 <leomet07@gmail.com>
2020-04-02 13:53:20 -0400

    What we need

--- large.c
+++ large.c
@@ -13,32 +13,11 @@ double total_incubation = 0;
 long amnt = 1;
 char *str_slice(char str[], int slice_from, int slice_to)
 {
-    // if a string is empty, returns nothing
-    if (str[0] == '\0')
-        return NULL;
 
     char *buffer;
     size_t str_len, buffer_len;
 
-    // for negative indexes "slice_from" must be less "slice_to"
-    if (slice_to < 0 && slice_from < slice_to)
-    {
-        str_len = strlen(str);
-
-        // if "slice_to" goes beyond permissible limits
-        if (abs(slice_to) > str_len - 1)
-            return NULL;
-
-        // if "slice_from" goes beyond permissible limits
-        if (abs(slice_from) > str_len)
-            slice_from = (-1) * str_len;
-
-        buffer_len = slice_to - slice_from;
-        str += (str_len + slice_from);
-
-        // for positive indexes "slice_from" must be more "slice_to"
-    }
-    else if (slice_from >= 0 && slice_to > slice_from)
+    if (slice_from >= 0 && slice_to > slice_from)
     {
         str_len = strlen(str);
 
@@ -48,11 +27,7 @@ char *str_slice(char str[], int slice_from, int slice_to)
 
         buffer_len = slice_to - slice_from;
         str += slice_from;
-
-        // otherwise, returns NULL
     }
-    else
-        return NULL;
 
     buffer = calloc(buffer_len, sizeof(char));
     strncpy(buffer, str, buffer_len);
@@ -98,6 +73,11 @@ void parse_array(cJSON *array)
                 // any kinf of usage with sentence will cause the program to crash after ~1500 files
                 //printf("Sentence: %s\n", sentence);
                 // ^^^^^^ example usage
+
+                //example of what i want
+                char sentence_two[] = "This is some text. ";
+                //all str funcs should work
+                printf("Sentence: %s%d\n", sentence_two, strlen(sentence_two));
 
                 //int sentlen = strlen(sentence);
                 /*
